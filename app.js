a1ef6958f11d1a5d78e0aef835c7cc59601c289a
AlonaCh <aliona.chubenko@gmail.com>
2022-11-02 15:12:03 +0200

    Added real time

--- app.js
+++ app.js
@@ -1,25 +1,68 @@
-function showTemperature(response){
-    console.log(response.data)
-    let tempElement = document.querySelector("#temperature");
-    tempElement.innerHTML =Math.round(response.data.main.temp);
-    
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = response.data.name;
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
 
-    let descriptionElement = document.querySelector("#description");
-    descriptionElement.innerHTML = response.data.weather[0].description;
+  let dateNumber = date.getDate();
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  } 
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wendsday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
 
-    let humidityElement = document.querySelector("#humidity");
-    humidityElement.innerHTML = response.data.main.humidity;
+  let months = [
+    "Jan",
+    "Feb",
+    "March",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Now",
+    "Dec",
+  ];
+let month = months[date.getMonth()];
 
-    let windElement = document.querySelector("#wind");
-    windElement.innerHTML = Math.round(response.data.wind.speed);
+  return `${day} ${dateNumber} ${month}, ${hours}:${minutes}`;
+}
 
+function showTemperature(response) {
+  console.log(response.data);
+  let tempElement = document.querySelector("#temperature");
+  tempElement.innerHTML = Math.round(response.data.main.temp);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "b7a3558dd4231bb7517fc8c9d13c79d4";
-let apiUrl =
-  `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=metric`;
 
-  console.log(apiUrl);
+console.log(apiUrl);
 axios.get(apiUrl).then(showTemperature);
